f820e684d492fed374513bc68c405006dad0bb65
wmc <2904863800@qq.com>
2017-02-15 19:28:08 +0800

    完整带说明API轮播图

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //jQuery 出让 $ 符号的使用权（也就是说，从这里开始 $ 将不是 jQuery，只能用变量 jQuery）
-jQuery.noConflict();
+//jQuery.noConflict();
 
 //zySlide（）只要轮播图的根标签（任何选择器都行）
-jQuery('.slide').zySlide({speed:1000}).css({
+$('.slide').zySlide({speed:1000}).css({
 	"border": "3px dotted red",
 	"backgroundColor": "gold"
 });
